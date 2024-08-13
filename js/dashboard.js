cb88e5efd698a328bbda2e5532433f12d0add0f8
DwiFirdaus08 <147034299+DwiFirdaus08@users.noreply.github.com>
2024-08-13 10:34:29 +0700

    add table

--- js/dashboard.js
+++ js/dashboard.js
@@ -4,8 +4,57 @@ get("https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/webhook/
 
 
 
-function responsefunction(result) {
- console.log(result);
+function responsefunction(items) {
+ console.log(items);
+ 
+ let tableRows = "";
+ let lastRowStyle = "bg-gray-50"; // Mulai dengan gaya pertama
+
+ items.forEach((item) => {
+   const rowStyle = lastRowStyle;
+   lastRowStyle = lastRowStyle === "bg-gray-50" ? "" : "bg-gray-50"; // Berganti gaya
+
+   tableRows += `
+     <tr class="text-xs ${rowStyle}">
+       <td class="flex px-4 py-3">
+         <div>
+           <p class="font-medium">${item.prohibited_items || "N/A"}</p>
+         </div>
+       </td>
+       <td class="font-medium">${item.destination || "N/A"}</td>
+       <td>
+         <div>
+           <!-- Ikon Edit -->
+           <a class="inline-block mr-2" href="edititem.html?id=${item.id}">
+             <i class="fas fa-edit" style="font-size: 18px; color: #382CDD;"></i>
+           </a>
+           
+           <!-- Ikon Delete -->
+           <a class="inline-block" href="#" onclick="deleteItemEn('${
+             item.id
+           }')">
+             <i class="fas fa-trash" style="font-size: 20px; color: #E85444;"></i>
+           </a>
+         </div>
+       </td>
+     </tr>
+   `;
+ });
+
+ document.getElementById("content-en").innerHTML = `
+   <table class="table-auto w-full">
+     <thead>
+       <tr class="text-xs text-gray-500 text-left">
+         <th class="font-medium">Items Name</th>
+         <th class="font-medium">Destination</th>
+         <th class="font-medium">Action</th>
+       </tr>
+     </thead>
+     <tbody class="visibility-item">
+       ${tableRows}
+     </tbody>
+   </table>
+ `;
 }
 
 
