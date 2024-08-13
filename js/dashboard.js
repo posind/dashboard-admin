5011b5da08994b8c3c9373a07c6e783b587d92f0
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-08-13 10:58:46 +0700

    buat fauction baru untuk get in

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,7 +3,7 @@ import {get}from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.0.4/api.js";
 get("https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/webhook/get/prohibited-items/en", responsefunction);
 
 
-
+// Fungsi untuk memuat konten Bahasa Inggris
 function responsefunction(items) {
  console.log(items);
  
@@ -57,8 +57,61 @@ function responsefunction(items) {
  `;
 }
 
+get("https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/webhook/get/item", loadBarang);
 
+// Fungsi untuk memuat konten Bahasa Indonesia
+function loadBarang(barangs) {
+  console.log(barangs);
+  
+  let tableRows = "";
+  let lastRowStyle = "bg-gray-50"; // Mulai dengan gaya pertama
 
+  barangs.forEach((barang) => {
+    const rowStyle = lastRowStyle;
+    lastRowStyle = lastRowStyle === "bg-gray-50" ? "" : "bg-gray-50"; // Berganti gaya
+
+    tableRows += `
+      <tr class="text-xs ${rowStyle}">
+        <td class="flex px-4 py-3">
+          <div>
+            <p class="font-medium">${barang.barang_terlarang || "N/A"}</p>
+          </div>
+        </td>
+        <td class="font-medium">${barang.destinasi || "N/A"}</td>
+        <td>
+          <div>
+            <!-- Ikon Edit -->
+            <a class="inline-block mr-2" href="editbrg.html?id=${barang.id}">
+              <i class="fas fa-edit" style="font-size: 18px; color: #382CDD;"></i>
+            </a>
+            
+            <!-- Ikon Delete -->
+            <a class="inline-block" href="#" onclick="deleteItemId('${
+              barang.id
+            }')">
+              <i class="fas fa-trash" style="font-size: 20px; color: #E85444;"></i>
+            </a>
+          </div>
+        </td>
+      </tr>
+    `;
+  });
+
+  document.getElementById("content-id").innerHTML = `
+    <table class="table-auto w-full">
+      <thead>
+        <tr class="text-xs text-gray-500 text-left">
+          <th class="font-medium">Nama Barang</th>
+          <th class="font-medium">Destinasi</th>
+          <th class="font-medium">Aksi</th>
+        </tr>
+      </thead>
+      <tbody class="visibility-item">
+        ${tableRows}
+      </tbody>
+    </table>
+  `;
+ }
 
 // Fungsi untuk menavigasi halaman dengan parameter dinamis
 function navigateTo(page) {
@@ -77,72 +130,6 @@ function tambahBarang() {
   navigateTo("crud/tambahbr.html");
 }
 
-// Fungsi untuk memuat konten Bahasa Inggris
-async function loadItems() {
-  try {
-    console.log("Loading items...");
-    const response = await fetch(
-      "https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/webhook/get/prohibited-items/en"
-    );
-    if (!response.ok) throw new Error("Network response was not ok");
-    const items = await response.json();
-    console.log("Items loaded:", items);
-
-    let tableRows = "";
-    let lastRowStyle = "bg-gray-50"; // Mulai dengan gaya pertama
-
-    items.forEach((item) => {
-      const rowStyle = lastRowStyle;
-      lastRowStyle = lastRowStyle === "bg-gray-50" ? "" : "bg-gray-50"; // Berganti gaya
-
-      tableRows += `
-        <tr class="text-xs ${rowStyle}">
-          <td class="flex px-4 py-3">
-            <div>
-              <p class="font-medium">${item.prohibited_items || "N/A"}</p>
-            </div>
-          </td>
-          <td class="font-medium">${item.destination || "N/A"}</td>
-          <td>
-            <div>
-              <!-- Ikon Edit -->
-              <a class="inline-block mr-2" href="edititem.html?id=${item.id}">
-                <i class="fas fa-edit" style="font-size: 18px; color: #382CDD;"></i>
-              </a>
-              
-              <!-- Ikon Delete -->
-              <a class="inline-block" href="#" onclick="deleteItemEn('${
-                item.id
-              }')">
-                <i class="fas fa-trash" style="font-size: 20px; color: #E85444;"></i>
-              </a>
-            </div>
-          </td>
-        </tr>
-      `;
-    });
-
-    document.getElementById("content-en").innerHTML = `
-      <table class="table-auto w-full">
-        <thead>
-          <tr class="text-xs text-gray-500 text-left">
-            <th class="font-medium">Items Name</th>
-            <th class="font-medium">Destination</th>
-            <th class="font-medium">Action</th>
-          </tr>
-        </thead>
-        <tbody class="visibility-item">
-          ${tableRows}
-        </tbody>
-      </table>
-    `;
-  } catch (error) {
-    console.error("Error loading items:", error);
-    document.getElementById(
-      "content-en"
-    ).innerHTML = `<p class="text-red-500">Failed to load items. Please try again later.</p>`;
-  }
-}
 
 // Fungsi untuk menghapus item Bahasa Inggris
 function deleteItemEn(id) {
@@ -165,71 +152,6 @@ function deleteItemEn(id) {
         console.error("Error:", error);
         alert("Error deleting item");
       });
-  }
-}
-
-// Fungsi untuk memuat konten Bahasa Indonesia
-async function loadBarang() {
-  try {
-    const response = await fetch(
-      "https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/webhook/get/item"
-    );
-    if (!response.ok) throw new Error("Network response was not ok");
-    const barangs = await response.json();
-
-    let tableRows = "";
-    let lastRowStyle = "bg-gray-50"; // Mulai dengan gaya pertama
-
-    barangs.forEach((barang) => {
-      const rowStyle = lastRowStyle;
-      lastRowStyle = lastRowStyle === "bg-gray-50" ? "" : "bg-gray-50"; // Berganti gaya
-
-      tableRows += `
-        <tr class="text-xs ${rowStyle}">
-          <td class="flex px-4 py-3">
-            <div>
-              <p class="font-medium">${barang.barang_terlarang || "N/A"}</p>
-            </div>
-          </td>
-          <td class="font-medium">${barang.destinasi || "N/A"}</td>
-          <td>
-            <div>
-              <!-- Ikon Edit -->
-              <a class="inline-block mr-2" href="editbrg.html?id=${barang.id}">
-                <i class="fas fa-edit" style="font-size: 18px; color: #382CDD;"></i>
-              </a>
-              
-              <!-- Ikon Delete -->
-              <a class="inline-block" href="#" onclick="deleteItemId('${
-                barang.id
-              }')">
-                <i class="fas fa-trash" style="font-size: 20px; color: #E85444;"></i>
-              </a>
-            </div>
-          </td>
-        </tr>
-      `;
-    });
-
-    document.getElementById("content-id").innerHTML = `
-      <table class="table-auto w-full">
-        <thead>
-          <tr class="text-xs text-gray-500 text-left">
-            <th class="font-medium">Nama Barang</th>
-            <th class="font-medium">Destinasi</th>
-            <th class="font-medium">Aksi</th>
-          </tr>
-        </thead>
-        <tbody class="visibility-item">
-          ${tableRows}
-        </tbody>
-      </table>
-    `;
-  } catch (error) {
-    console.error("Error loading barangs:", error);
-    document.getElementById(
-      "content-id"
-    ).innerHTML = `<p class="text-red-500">Gagal memuat barang. Coba lagi nanti.</p>`;
   }
 }
 
