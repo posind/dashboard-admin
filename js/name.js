d17d020afc09d3a5680f963a94151724018dd80a
DwiFirdaus08 <kirey.zaindeltaperguson@gmail.com>
2024-11-22 10:26:32 +0700

    coboa aktifkan function get nama

--- js/name.js
+++ js/name.js
@@ -3,15 +3,15 @@ import { setInner } from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.5/croo
 import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/api@0.0.7/croot.js";
 import { redirect } from "https://cdn.jsdelivr.net/gh/jscroot/url@0.0.9/croot.js";
 
-// function checkLoginAndFetchData() {
-//   const loginToken = getCookie("login");
-//   if (!loginToken) {
-//     redirect("../"); 
-//     return; 
-//   }
+function checkLoginAndFetchData() {
+  const loginToken = getCookie("login");
+  if (!loginToken) {
+    redirect("../"); 
+    return; 
+  }
 
-//   getJSON("https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/pibackend/data/user", "login", loginToken, responseFunction);
-// }
+  getJSON("https://asia-southeast2-civil-epigram-429004-t8.cloudfunctions.net/pibackend/data/user", "login", loginToken, responseFunction);
+}
 
 function responseFunction(result) {
   if (result.status === 200) {
